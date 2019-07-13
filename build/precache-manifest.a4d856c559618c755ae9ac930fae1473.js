654433e4abc6dd6289450acfa798c808713f5c0e
Erik Hagreis <erik.hagreis@gmail.com>
2019-07-13 14:14:52 +0200

    chore: add build to published package

---
+++ build/precache-manifest.a4d856c559618c755ae9ac930fae1473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e07a6fc72783bc9afd5ae7d3ed536e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad24a80f36fb023e0013",
+    "url": "./static/css/main.d0271169.chunk.css"
+  },
+  {
+    "revision": "a7a708798d9c96cb2ee3",
+    "url": "./static/js/2.e1ee5adf.chunk.js"
+  },
+  {
+    "revision": "ad24a80f36fb023e0013",
+    "url": "./static/js/main.fe7023de.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
